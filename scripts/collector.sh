d4a97da2aece93c17157a8846f73323859a0629a
Var091 <61744939+Var091@users.noreply.github.com>
2021-09-12 20:22:13 +0200

    move collector.sh to scripts/collector.sh

---
+++ scripts/collector.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#Variables
+REPORT_PATH=$1/$(date +%d-%b-%H:%M)
+echo "The reports will be saved in: $REPORT_PATH"
+#Cleaning-up
+if test -f "$REPORT_PATH"; then
+    cat /dev/null > $REPORT_PATH
+fi
+kubectl delete pod/collector
+
+
+for node in $(kubectl get nodes -o jsonpath="{.items[*].metadata.name}")
+
+do
+
+cat << EOF > collector-pod.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: collector
+  labels:
+    app: collector
+spec:
+  hostNetwork: true
+  nodeName: $node
+  serviceAccountName: alvaro-test
+  containers:
+  - image: liubowei/netstat
+    command:
+      - "sleep"
+      - "infinity"
+    imagePullPolicy: IfNotPresent
+    name: collector
+  restartPolicy: Always
+EOF
+
+echo "Ports opened in Node: $node" >> $REPORT_PATH
+
+kubectl apply -f collector-pod.yaml
+sleep 5
+kubectl exec collector -- bash -c "netstat -putan|grep LISTEN|grep 0.0.0.0.0"|awk '{print $4}' | awk -F ':' '{print $2}' >> $REPORT_PATH
+kubectl delete pod collector
+
+done
